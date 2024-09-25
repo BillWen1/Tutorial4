3150c5936acbeabb7d0f94d5c49c6bb76f13c305
BillWen1 <w1120212910@gmail.com>
2024-09-25 16:06:33 +0800

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,9 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+  @Test
+    void testAddition1() {
+        assertEquals(2, c.subtract(4,2));
+    }
 
 }
